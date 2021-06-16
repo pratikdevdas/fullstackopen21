a2a283f8f17f0e68df17c9c1251b809047d58a8e
Pratik <dasdev.pratik@gmail.com>
2021-06-16 21:17:03 +0530

    4.5 complete

--- part4/bloglistbackend/utils/list_helper.js
+++ part4/bloglistbackend/utils/list_helper.js
@@ -2,18 +2,20 @@ const dummy = (blogs) => {
     return 1;
   }
 
-  const totalLikes = (array) => {
-    const reducer = (sum, item) => {
-          return sum + item.likes
-    }
-    return array.reduce(reducer, 0)
+  const totalLikes = (blogs) => {
+    const reducer = (sum, item) => sum + item.likes
+    return blogs.reduce(reducer, 0)
   }
    
-  const favouriteBlog = (array) => {
-    const reducer = (sum, item) => {
-          return sum + item.likes
-    }
-    return array.reduce(reducer, 0)
+  const favouriteBlog = (blogs) => {
+    const maxLike = blogs.reduce(
+      (max, blog) => (blog.likes > max ? blog.likes : max),
+      blogs[0].likes
+    );
+    console.log(maxLike);
+    const obj = blogs.find(o => o.likes === maxLike);
+    const val = {title:obj.title,author:obj.author,likes:obj.likes}
+    return val
   }
   module.exports = {
     dummy,
