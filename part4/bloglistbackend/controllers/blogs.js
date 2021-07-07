ae5d648b0d2a789e7b7df6ba0b4e7117e168acd2
Pratik <dasdev.pratik@gmail.com>
2021-07-07 20:07:45 +0530

    ex 4.1 and 4.2

---
+++ part4/bloglistbackend/controllers/blogs.js
@@ -0,0 +1,27 @@
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+blogsRouter.get('/', (request, response) => {
+    response.send('<h1>Hellosdcddsf World!</h1>')
+  })
+
+  blogsRouter.get('/api/blogs', (request, response) => {
+    Blog
+      .find({})
+      .then(blogs => {
+        response.json(blogs)
+      })
+  })
+  
+  blogsRouter.post('/api/blogs', (request, response) => {
+    const blog = new Blog(request.body)
+  
+    blog
+      .save()
+      .then(result => {
+        response.status(201).json(result)
+      })
+  })
+  
+
+module.exports = blogsRouter
