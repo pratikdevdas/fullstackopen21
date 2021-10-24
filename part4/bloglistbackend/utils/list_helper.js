54b2eaa3f5834227c46a61efafec1f55a3c0296e
“Rev” <dasdev.pratik@gmail.com>
2021-10-25 02:53:00 +0530

    4.3 done

--- part4/bloglistbackend/utils/list_helper.js
+++ part4/bloglistbackend/utils/list_helper.js
@@ -1,6 +1,17 @@
-const dummy = (blogs) => 1
+const dummy = () => 1
 
+const totalLikes = (blogs) => {
+    const reducer = (sum,blog) => {
+        console.log(reducer)
+        return sum + blog.likes
+    }
+    return blogs.lemgth === 0
+        ? 0
+        : blogs.reduce(reducer,0)
+
+}
 
 module.exports={
-    dummy
+    dummy,
+    totalLikes
 }
