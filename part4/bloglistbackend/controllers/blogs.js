5b84275330da1e2266226db375139233a232e275
revenant <dasdev.pratik@gmail.com>
2021-12-04 12:24:16 +0530

    4.19 complete

--- part4/bloglistbackend/controllers/blogs.js
+++ part4/bloglistbackend/controllers/blogs.js
@@ -1,3 +1,5 @@
+const jwt = require('jsonwebtoken')
+const { error } = require('npmlog')
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 const User = require('../models/user')
@@ -13,17 +15,29 @@ blogsRouter.get('/:id', async(request, response) => {
     response.json(blog)
 })
 
+const getTokenFrom = request => {
+    const authorization = request.get('authorization')
+    if(authorization && authorization.toLowerCase().startsWith('bearer ')){
+        return authorization.substring(7)
+    }
+    return null
+}
+
 blogsRouter.post('/', async(request, response) => {
     const body = request.body
-
-    const user = await User.findById(body.userId)
+    const token = getTokenFrom(request)
+    const decodedToken = jwt.verify(token, process.env.SECRET)
+    if(!token || !decodedToken.id){
+        return response.status(401).json({ error:'token missing or invalid' })
+    }
+    const user = await User.findById(decodedToken.id)
 
     const blog = new Blog({
         title: body.title,
         author: body.author,
         url: body.url,
         likes: body.likes,
-        user: user
+        user: user._id
     })
 
     const savedBlog = await blog.save()
