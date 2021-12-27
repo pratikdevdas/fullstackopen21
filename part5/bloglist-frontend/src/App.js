e71304e87b1eb4a18f1eb6d71b17f4a777eb9c0a
rev <dasdev.pratik@gmail.com>
2021-12-27 09:46:39 +0530

    create with token forntend

--- part5/bloglist-frontend/src/App.js
+++ part5/bloglist-frontend/src/App.js
@@ -5,9 +5,13 @@ import loginService from './services/login'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [newTitle, setNewTitle] = useState("")
+  const [newAuthor, setNewAuthor] = useState("")
+  const [newUrl, setNewUrl] = useState("")
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
+
 // effect to get blogs
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -40,12 +44,50 @@ useEffect(() => {
       )
       blogService.setToken(user.token)
 
+      // for token change to call method
+      blogService.setToken(user.token)
       setUser(user)
       setUsername('')
       setPassword('')
     } catch (exception){
       console.log('error hogaya')
     }
+  }
+
+  const handleTitle = (event) => {
+    setNewTitle(event.target.value)   
+  }
+
+  const handleAuthor = (event) => {
+    setNewAuthor(event.target.value)   
+  }
+
+  const handleUrl = (event) => {
+    setNewUrl(event.target.value)   
+    console.log(event.setNewUrl)
+  }
+
+  const blogAdder = (event) => {
+    event.preventDefault()
+    console.log('created with title', newTitle)
+    const newBlog = {
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl,
+    }
+    if(newBlog.title.length<4)
+    {
+      return console.log('give longer value')
+    }
+    console.log('suceed')
+    blogService.create(newBlog)
+    .then(response=>{
+      setBlogs(blogs.concat(response))
+      setNewTitle("")
+      setNewAuthor("")
+      setNewUrl("")
+      console.log(response)
+    })
   }
 
   const loginForm = () => {
@@ -88,16 +130,44 @@ useEffect(() => {
     return (<div>
       <button onClick={()=>{setUser(null)}}>logOut</button>
     </div>)
+
   }
   return (
+
     <div>
+
+      <section className='header'>
+      <h2>blogs</h2>
      {user.name} loggedin 
      {logOut()}
-      <h2>blogs</h2>
+     </section>
+
+     <section className="newBlogCreation">
+        <h2>createNew</h2>
+         <div className="inputForm">
+            <form onSubmit={blogAdder}>
+              <div>
+                Title: <input value={newTitle} onChange={handleTitle}/>
+              </div>
+              <div>
+                author: <input value={newAuthor} onChange={handleAuthor}/>
+              </div>
+              <div>
+                url: <input value={newUrl} onChange={handleUrl}/>
+              </div>
+              <div>     
+                <button type="submit">add</button>
+              </div>
+            </form>
+          </div>
+      </section>
+
+     <section className="blogsCreated">
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
-      
+     </section>
+
     </div>
   )
 }
