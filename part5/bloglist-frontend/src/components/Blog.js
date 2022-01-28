0f629f47c760d0bc16e2a7961a9389d1d79cb8b6
rev <dasdev.pratik@gmail.com>
2022-01-28 21:28:17 +0530

    5.7 done

--- part5/bloglist-frontend/src/components/Blog.js
+++ part5/bloglist-frontend/src/components/Blog.js
@@ -1,8 +1,27 @@
 import React from 'react'
-const Blog = ({blog}) => (
-  <div>
-    {blog.title} {blog.author}
+import Togglable from './Togglable'
+
+const Blog = ({blog}) => {
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5,
+  }
+
+  return (
+  <div style={blogStyle}>
+         {blog.title}
+         <Togglable buttonLabel='show'>
+         <div>
+          {blog.author} 
+         </div>
+         <div>
+         {blog.url}
+         </div>
+       </Togglable>
   </div>  
-)
+)}
 
 export default Blog
