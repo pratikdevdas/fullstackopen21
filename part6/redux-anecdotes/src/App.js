a96af1ee984be0388660a0636e87d79125b71172
rev <dasdev.pratik@gmail.com>
2022-03-04 15:46:33 +0530

    6.5 done

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -17,11 +17,15 @@ const App = () => {
     dispatch(createAnecdote(content))
   }
 
+  // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/sort#description
+  // https://github.com/pratikdevdas/fullstackopen21/blob/27b4768b86643d66d8459a9d071fc66d617ae8f1/part5/bloglist-frontend/src/App.js#L156
+
+  const sortedAnecdotes = anecdotes.sort((a,b)=>b.votes - a.votes)
   console.log(anecdotes)
     return (
     <div>
       <h2>Anecdotes</h2>
-      {anecdotes.map(anecdote =>
+      {sortedAnecdotes.map(anecdote =>
         <div key={anecdote.id}>
           <div>
             {anecdote.content}
