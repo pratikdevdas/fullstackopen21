79f04905b5e57579cb4a1df89fa54cfd16fa4ea4
rev <dasdev.pratik@gmail.com>
2022-03-08 17:10:42 +0530

    6.9 done

--- part6/redux-anecdotes/src/store.js
+++ part6/redux-anecdotes/src/store.js
@@ -1,8 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
 import anecdoteReducer from './reducers/anecdoteReducer'
 
-
-
 const store = configureStore({
     reducer: {
       anecdotes: anecdoteReducer,
