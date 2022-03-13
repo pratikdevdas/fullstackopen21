f7dab40ab1704391aaa3686edf9133b886b9ae8c
Pratik <dasdev.pratik@gmail.com>
2022-03-13 20:21:01 +0530

    6.13.3(adding notes with set notes and useEffect)

--- part6/redux-anecdotes/src/App.js
+++ part6/redux-anecdotes/src/App.js
@@ -1,9 +1,18 @@
+import { useEffect } from 'react'
 import AnecdoteForm from './components/AnecdoteForm'
 import AnecdoteList from './components/AnecdoteList'
 import Notification from './components/Notification'
 import Filter from './components/Filter'
+import backendService from './services/backend'
+import { setNotes } from './reducers/anecdoteReducer'
+import { useDispatch } from 'react-redux'
 
 const App = () => {
+  const dispatch = useDispatch()
+  useEffect(()=> {
+    backendService.getAll().then(anecdote => dispatch(setNotes(anecdote)))
+  },[]) // eslint-disable-line react-hooks/exhaustive-deps
+
     return (
     <div>
       <Notification/>
