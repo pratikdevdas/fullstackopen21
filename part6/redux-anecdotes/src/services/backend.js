328b8eaace071eac0d540bf370a507272b2de17b
Revenant <dasdev.pratik@gmail.com>
2022-03-20 16:07:34 +0530

    6.19 done

--- part6/redux-anecdotes/src/services/backend.js
+++ part6/redux-anecdotes/src/services/backend.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/anecdotes'
+const baseUrl = 'http://localhost:3005/anecdotes'
 
 const getAll = async()=>{
     const response = await axios.get(baseUrl)
