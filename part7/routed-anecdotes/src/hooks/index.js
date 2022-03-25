1e81001b1b0d4a44ef910b7b720de11ebf10870f
Revenant <dasdev.pratik@gmail.com>
2022-03-25 17:29:11 +0530

    ex 7.6 done

--- part7/routed-anecdotes/src/hooks/index.js
+++ part7/routed-anecdotes/src/hooks/index.js
@@ -3,13 +3,21 @@ import {useState} from "react";
 export const useField = (type) => {
     const [value, setValue] = useState('')
 
+    // in hooks its different like we pass the function as valid props
     const onChange = (event) => {
+        console.log(event.target.value)
         setValue(event.target.value)
     }
+    
 
+    const reset = (event)=>{
+        // console.log(event.target.value)
+        setValue('')
+    }
     return{
         value,
         type,
-        onChange
+        onChange,
+        reset,
     }
 }
