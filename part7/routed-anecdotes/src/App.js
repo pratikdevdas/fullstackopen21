a467f97a4dcd5f00353d3b101269514357dc1e98
Revenant <dasdev.pratik@gmail.com>
2022-03-25 17:29:16 +0530

    ex 7.6 done

--- part7/routed-anecdotes/src/App.js
+++ part7/routed-anecdotes/src/App.js
@@ -1,86 +1,117 @@
-import React, { useState } from 'react'
-import { Routes, Route, Link, useParams, useNavigate} from 'react-router-dom'
-import { useField } from './hooks'
+import React, { useState } from "react";
+import { Routes, Route, Link, useParams, useNavigate } from "react-router-dom";
+import { useField } from "./hooks";
 
 const Menu = () => {
   const padding = {
-    paddingRight: 5
-  }
+    paddingRight: 5,
+  };
   return (
     <div>
-     <Link to="/" style={padding}>home </Link>
-          <Link to="/createnew" style={padding}>create new </Link>
-          <Link to="/about" style={padding}>about</Link>
+      <Link to="/" style={padding}>
+        home{" "}
+      </Link>
+      <Link to="/createnew" style={padding}>
+        create new{" "}
+      </Link>
+      <Link to="/about" style={padding}>
+        about
+      </Link>
     </div>
-  )
-}
+  );
+};
 
 const Anecdote = ({ anecdotes }) => {
-  const id = useParams().id
+  const id = useParams().id;
   // console.log(typeof(id))
-  const anecdote = anecdotes.find((n) => { 
-    return Number(n.id) === Number(id)})
+  const anecdote = anecdotes.find((n) => {
+    return Number(n.id) === Number(id);
+  });
   // console.log(anecdote)
-  console.log(id)
-  return(
+  console.log(id);
+  return (
     <div>
-      <h2>{anecdote.content} by {anecdote.author}  </h2>
+      <h2>
+        {anecdote.content} by {anecdote.author}{" "}
+      </h2>
       has {anecdote.vote} votes
     </div>
-  )
-}
+  );
+};
 
 const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} ><Link to={`/anecdote/${anecdote.id}`}>{anecdote.content} </Link></li>)}
+      {anecdotes.map((anecdote) => (
+        <li key={anecdote.id}>
+          <Link to={`/anecdote/${anecdote.id}`}>{anecdote.content} </Link>
+        </li>
+      ))}
     </ul>
   </div>
-)
+);
 
 const About = () => (
   <div>
     <h2>About anecdote app</h2>
     <p>According to Wikipedia:</p>
 
-    <em>An anecdote is a brief, revealing account of an individual person or an incident.
-      Occasionally humorous, anecdotes differ from jokes because their primary purpose is not simply to provoke laughter but to reveal a truth more general than the brief tale itself,
-      such as to characterize a person by delineating a specific quirk or trait, to communicate an abstract idea about a person, place, or thing through the concrete details of a short narrative.
-      An anecdote is "a story with a point."</em>
+    <em>
+      An anecdote is a brief, revealing account of an individual person or an
+      incident. Occasionally humorous, anecdotes differ from jokes because their
+      primary purpose is not simply to provoke laughter but to reveal a truth
+      more general than the brief tale itself, such as to characterize a person
+      by delineating a specific quirk or trait, to communicate an abstract idea
+      about a person, place, or thing through the concrete details of a short
+      narrative. An anecdote is "a story with a point."
+    </em>
 
-    <p>Software engineering is full of excellent anecdotes, at this app you can find the best and add more.</p>
+    <p>
+      Software engineering is full of excellent anecdotes, at this app you can
+      find the best and add more.
+    </p>
   </div>
-)
+);
 
 const Footer = () => (
   <div>
-    Anecdote app for <a href='https://courses.helsinki.fi/fi/tkt21009'>Full Stack -websovellus+kehitys</a>.
-
-    See <a href='https://github.com/fullstack-hy/routed-anecdotes/blob/master/src/App.js'>https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js</a> for the source code.
+    Anecdote app for{" "}
+    <a href="https://courses.helsinki.fi/fi/tkt21009">
+      Full Stack -websovellus+kehitys
+    </a>
+    . See{" "}
+    <a href="https://github.com/fullstack-hy/routed-anecdotes/blob/master/src/App.js">
+      https://github.com/fullstack-hy2019/routed-anecdotes/blob/master/src/App.js
+    </a>{" "}
+    for the source code.
   </div>
-)
+);
 
-const CreateNew = ({addNew,handleNotification}) => {
+const CreateNew = ({ addNew, handleNotification }) => {
+  const content = useField("text", "reset");
+  const author = useField("text", "reset");
+  const info = useField("text", "reset");
+  // console.log({...content}===content,{...content},content);
 
-  const content = useField('text')
-  const author = useField('text')
-  const info = useField('text')
-
-    // console.log({...content}===content,{...content},content);
-    
   const handleSubmit = (e) => {
-    e.preventDefault()
+    e.preventDefault();
     addNew({
       content: content.value,
       author: author.value,
-      info:info.value,
-      votes: 0
-    })
+      info: info.value,
+      votes: 0,
+    });
     handleNotification({
-      content: content.value
-    })
-  }
+      content: content.value,
+    });
+  };
+
+  const handleClear = () => {
+    content.reset();
+    author.reset();
+    info.reset();
+  };
 
   return (
     <div>
@@ -88,7 +119,7 @@ const CreateNew = ({addNew,handleNotification}) => {
       <form onSubmit={handleSubmit}>
         <div>
           content
-          <input {...content}/>
+          <input {...content} />
         </div>
         <div>
           author
@@ -100,81 +131,93 @@ const CreateNew = ({addNew,handleNotification}) => {
         </div>
         <button>create</button>
       </form>
+      <button onClick={handleClear}>reset</button>
     </div>
-  )
-
-}
+  );
+};
 
 const App = () => {
   const [anecdotes, setAnecdotes] = useState([
     {
-      content: 'If it hurts, do it more often',
-      author: 'Jez Humble',
-      info: 'https://martinfowler.com/bliki/FrequencyReducesDifficulty.html',
+      content: "If it hurts, do it more often",
+      author: "Jez Humble",
+      info: "https://martinfowler.com/bliki/FrequencyReducesDifficulty.html",
       votes: 0,
-      id: '1'
+      id: "1",
     },
     {
-      content: 'Premature optimization is the root of all evil',
-      author: 'Donald Knuth',
-      info: 'http://wiki.c2.com/?PrematureOptimization',
+      content: "Premature optimization is the root of all evil",
+      author: "Donald Knuth",
+      info: "http://wiki.c2.com/?PrematureOptimization",
       votes: 0,
-      id: '2'
-    }
-  ])
+      id: "2",
+    },
+  ]);
 
-  const [notification, setNotification] = useState('')
-  const homeAfterCreate = useNavigate()
+  const [notification, setNotification] = useState("");
+  const homeAfterCreate = useNavigate();
 
   const handleNotification = (anecdote) => {
-    setNotification(`a new anecdote created ${anecdote.content}`)
-    setTimeout(()=>{
-      setNotification('')
-    },3000)
-    homeAfterCreate('/')
-  }
+    setNotification(`a new anecdote created ${anecdote.content}`);
+    setTimeout(() => {
+      setNotification("");
+    }, 3000);
+    homeAfterCreate("/");
+  };
 
   const addNew = (anecdote) => {
-    console.log(anecdote)
-    anecdote.id = (Math.random() * 10000).toFixed(0)
-    setAnecdotes(anecdotes.concat(anecdote))
-  }
+    console.log(anecdote);
+    anecdote.id = (Math.random() * 10000).toFixed(0);
+    setAnecdotes(anecdotes.concat(anecdote));
+  };
 
-  const anecdoteById = (id) =>
-    anecdotes.find(a => a.id === id)
+  const anecdoteById = (id) => anecdotes.find((a) => a.id === id);
 
   const vote = (id) => {
-    const anecdote = anecdoteById(id)
+    const anecdote = anecdoteById(id);
 
     const voted = {
       ...anecdote,
-      votes: anecdote.votes + 1
-    }
+      votes: anecdote.votes + 1,
+    };
 
-    setAnecdotes(anecdotes.map(a => a.id === id ? voted : a))
-  }
+    setAnecdotes(anecdotes.map((a) => (a.id === id ? voted : a)));
+  };
 
   return (
     <div>
       <h1>Software anecdotes</h1>
-        <div> 
+      <div>
         <Menu />
         {notification}
-        </div>
-         
-         <Routes>
-           <Route path="/about" element={<About />}></Route>
+      </div>
 
-           <Route path="/anecdote/:id" element={<Anecdote anecdotes={anecdotes} />}></Route>
-           
-           <Route path="/createnew" element={<CreateNew addNew={addNew} handleNotification={handleNotification} />}></Route>
-           
-           <Route path="/" element={<AnecdoteList anecdotes={anecdotes} />}></Route>
-         </Routes>
+      <Routes>
+        <Route path="/about" element={<About />}></Route>
+
+        <Route
+          path="/anecdote/:id"
+          element={<Anecdote anecdotes={anecdotes} />}
+        ></Route>
+
+        <Route
+          path="/createnew"
+          element={
+            <CreateNew
+              addNew={addNew}
+              handleNotification={handleNotification}
+            />
+          }
+        ></Route>
+
+        <Route
+          path="/"
+          element={<AnecdoteList anecdotes={anecdotes} />}
+        ></Route>
+      </Routes>
       <Footer />
-      
     </div>
-  )
-}
+  );
+};
 
 export default App;
