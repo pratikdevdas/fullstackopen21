087f597a227846a3c2845b7e5eed20e875e4a94a
Revenant <dasdev.pratik@gmail.com>
2022-12-29 23:33:51 +0530

    9.2 (refactored and added interface)

--- part9/setup/excerciseCalculator.ts
+++ part9/setup/excerciseCalculator.ts
@@ -1,17 +1,26 @@
-function calculateExcercises(daily: Array<number>, target: number) {
+interface Result{
+    periodLength: number,
+    trainingDays: number,
+    sucess:boolean,
+    rating:number,
+    ratingDescription:string,
+    target:number,
+    average:number
+}
+
+function calculateExcercises(daily: Array<number>, target: number):Result {
     const arr: Array<Number> = []
     daily.forEach((arrayValue) => arrayValue === 0 ? null : arr.push(arrayValue))
     let sum: number = 0;
     daily.forEach((arrayValue) => {
-        console.log(sum += arrayValue)
-        return sum
+        return sum += arrayValue
+        
     })
     const average = sum / daily.length
     let rating, sucess;
     if (average<target){
         sucess = false;
         rating = Math.floor(Math.random()*10-5)> 0 ? 1 : 2
-        console.log(rating)
     } else {
         sucess = true;
         rating = 3
@@ -20,7 +29,7 @@ function calculateExcercises(daily: Array<number>, target: number) {
     let ratingDescription;
     switch (rating) {
         case 1:
-            ratingDescription = "very bad, impronvement"
+            ratingDescription = "very bad, improvement"
             break;
         case 2:
             ratingDescription = "not too bad but could be better"
@@ -32,7 +41,7 @@ function calculateExcercises(daily: Array<number>, target: number) {
             break;
     }
     
-    const result = {
+    return {
         periodLength: daily.length,
         trainingDays: arr.length,
         sucess,
@@ -41,7 +50,6 @@ function calculateExcercises(daily: Array<number>, target: number) {
         target,
         average
     }
-    return result
 }
 
 console.log(calculateExcercises([3, 0, 2, 4.5, 0, 3, 1], 1))
