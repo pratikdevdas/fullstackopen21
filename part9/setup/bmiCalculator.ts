481ac790bd4ae6b989c87ef9cd476155061aac7a
Revenant <dasdev.pratik@gmail.com>
2022-12-31 01:10:08 +0530

    excercise 9.3 done

--- part9/setup/bmiCalculator.ts
+++ part9/setup/bmiCalculator.ts
@@ -1,3 +1,22 @@
+interface bmiValues {
+    value1: number;
+    value2: number;
+}
+
+const parseArguments = (args: Array<string>): bmiValues => {
+    if (args.length < 4) throw new Error('Not enough arguments');
+    if (args.length > 4) throw new Error('Too many arguments');
+  
+    if (!isNaN(Number(args[2])) && !isNaN(Number(args[3]))) {
+      return {
+        value1: Number(args[2]),
+        value2: Number(args[3])
+      }
+    } else {
+      throw new Error('Provided values were not numbers!');
+    }
+  }
+
 const calculateBmi = (height: number, mass: number): string => {
     const res = mass / (height * height) * 10000;
     console.log(res);
@@ -23,4 +42,15 @@ const calculateBmi = (height: number, mass: number): string => {
     }
 }
 
-console.log(calculateBmi(177, 82))
+
+
+try {
+    const {value1, value2} = parseArguments(process.argv)
+    console.log(calculateBmi(value1, value2))
+} catch (error:unknown) {
+    let errorMessage = "Something bad happened"
+    if (error instanceof Error){
+        errorMessage += 'Error' + error.message;
+    }
+    console.log(errorMessage)
+}
--- part9/setup/excerciseCalculator.ts
+++ part9/setup/excerciseCalculator.ts
@@ -8,6 +8,31 @@ interface Result{
     average:number
 }
 
+interface excerciseValue{
+    value1: Array<number>,
+    value2: number
+}
+
+function arguments(args:Array<string>):excerciseValue{
+    let array:Array<number> = []
+    let target 
+        if(args.length < 4){
+            throw new Error ("Not Enough arguement")
+        }
+        args.forEach((value)=>{
+            if(!isNaN(Number(value))){
+                array.push(Number(value))
+            }
+        })     
+
+        target =  array.shift()
+        if(array.length === 0){
+            throw new Error('Invalid Arguments')
+        }
+        return {value1:array, value2:target}
+}
+
+
 function calculateExcercises(daily: Array<number>, target: number):Result {
     const arr: Array<Number> = []
     daily.forEach((arrayValue) => arrayValue === 0 ? null : arr.push(arrayValue))
@@ -35,7 +60,7 @@ function calculateExcercises(daily: Array<number>, target: number):Result {
             ratingDescription = "not too bad but could be better"
             break;
             case 3:
-            ratingDescription = "very sexy"
+            ratingDescription = "very Nice, appreciating hardwork"
             break;
         default:
             break;
@@ -52,4 +77,16 @@ function calculateExcercises(daily: Array<number>, target: number):Result {
     }
 }
 
-console.log(calculateExcercises([3, 0, 2, 4.5, 0, 3, 1], 1))
+
+
+
+try {
+    const {value1, value2}= arguments(process.argv)    
+    console.log(calculateExcercises(value1,value2))
+} catch (error) {
+    let errorMessage = "Something Bad Happened"
+    if( error instanceof Error){
+        errorMessage += ' Error ' + error.message
+    }
+    throw new Error(errorMessage);
+}
