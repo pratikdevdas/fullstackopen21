f1ef0202258fb547bd11e2d680cd8e2f741e0d35
Revenant <dasdev.pratik@gmail.com>
2023-02-15 23:39:05 +0530

    9.26 done
    1) working on the legacy part since that what I started with

--- part9/patientor/src/components/PatientPage.tsx
+++ part9/patientor/src/components/PatientPage.tsx
@@ -3,9 +3,13 @@ import { useParams } from 'react-router-dom';
 import { useStateValue, singlePatient } from "../state";
 import axios from 'axios';
 import { apiBaseUrl } from '../constants';
-import { Patient } from '../types';
+import { Entry, Patient, HealthCheckEntry, HospitalEntry, OccupationalHealthcareEntry } from '../types';
 import MaleIcon from '@mui/icons-material/Male';
 import FemaleIcon from '@mui/icons-material/Female';
+import HealingIcon from '@mui/icons-material/Healing';
+import LocalHospitalIcon from '@mui/icons-material/LocalHospital';
+import MedicalServicesIcon from '@mui/icons-material/MedicalServices';
+
 const PatientPage = () => {
 	const [{ patients }, dispatch] = useStateValue();
 	const [{ diagnosis }] = useStateValue();
@@ -20,7 +24,7 @@ const PatientPage = () => {
 				const { data: singlePatientFromApi } = await axios.get<Patient>(
 					// eslint-disable-next-line @typescript-eslint/restrict-template-expressions
 					`${apiBaseUrl}/patients/${id}`
-					
+
 				);
 				dispatch(singlePatient(singlePatientFromApi));
 			} catch (e) {
@@ -47,14 +51,85 @@ const PatientPage = () => {
 			<p>occupation : {value?.occupation}</p>
 
 			<h3>entries</h3>
-			{value?.entries.map(n => <>{n.date}
-				<div>{n.description}</div>
-				{n.diagnosisCodes?.map(d => <li key={d}>{d}
-				{diagnosis[d] && <> {diagnosis[d].name}</>}
-				</li>)}
-			</>)}
+			{value?.entries.map(n => <EntryDetails key={n.id} entry={n}>
+			</EntryDetails>)}
 		</div>
 	);
 };
 
 export default PatientPage;
+
+//  do  not use the following type label in new react projects
+const EntryDetails: React.FC<{ entry: Entry }> = ({ entry }) => {
+	console.log(entry);
+	switch (entry.type) {
+		case "Hospital":
+			return <Hospital entry={entry} />;
+		case "OccupationalHealthcare":
+			return <OccupationalHealthcare entry={entry} />;
+		case "HealthCheck":
+			return <HealthCheck entry={entry} />;
+		default:
+			return null;
+	}
+};
+
+
+const borderStyle = {
+	padding: "10px",
+	border: '2px solid black',
+	borderRadius: '10px 	'
+};
+const textDescription = {
+	fontSize: 'italic'
+};
+
+const Hospital: React.FC<{ entry: HospitalEntry }> = ({ entry }) => {
+	return <div style={borderStyle}>
+		<p>{entry.date}
+			<LocalHospitalIcon />
+		</p>
+		<div style={textDescription} >{entry.description}</div>
+		<div> <b>Discharge date:</b> {entry.discharge.date} <b>Criteria:</b>{entry.discharge.criteria}</div>
+		<div>diagnose by {entry.specialist}</div>
+	</div>;
+};
+
+const OccupationalHealthcare: React.FC<{ entry: OccupationalHealthcareEntry }> = ({ entry }) => {
+	return <div style={borderStyle}>
+		<p>{entry.date}
+			<HealingIcon />
+		</p>
+		<div style={textDescription} >{entry.description}  {entry.employerName}</div>
+		<div>diagnose by {entry.specialist}</div>
+	</div>;
+};
+
+
+
+const HealthCheck: React.FC<{ entry: HealthCheckEntry }> = ({ entry }) => {
+	return <div style={borderStyle}>
+		<p>{entry.date}
+			<MedicalServicesIcon />
+		</p>
+		<div style={textDescription} >{entry.description}</div>
+		<div> <HealthCheckHeart heart={entry.healthCheckRating} /></div>
+		<div>diagnose by {entry.specialist}</div>
+	</div>;
+};
+
+
+function HealthCheckHeart({ heart }: { heart: number }) {
+	switch (heart) {
+		case 0:
+			return <div>❤️</div>;
+		case 1:
+			return <div>💛</div>;
+		case 2:
+			return <div>💙</div>;
+		case 3:
+			return <div>🤎</div>;
+		default:
+			return null;
+	}
+}
--- part9/patientor/src/types.ts
+++ part9/patientor/src/types.ts
@@ -34,7 +34,7 @@ export enum HealthCheckRating {
   'HighRisk' = 2,
   'CriticalRisk' = 3
 }
-interface HealthCheckEntry extends BaseEntry {
+export interface HealthCheckEntry extends BaseEntry {
   type: 'HealthCheck';
   healthCheckRating: HealthCheckRating;
 }
@@ -42,7 +42,7 @@ interface Discharge {
   date: string,
   criteria: string
 }
-interface HospitalEntry extends BaseEntry {
+export interface HospitalEntry extends BaseEntry {
   type: 'Hospital';
   discharge: Discharge;
 }
@@ -51,9 +51,10 @@ interface SickLeave {
   startDate: string;
   endDate: string;
 }
-interface OccupationalHealthcareEntry extends BaseEntry {
+export interface OccupationalHealthcareEntry extends BaseEntry {
   type: 'OccupationalHealthcare',
   employerName: string;
   sickLeave?: SickLeave
 }
+
 export type Entry = HospitalEntry | OccupationalHealthcareEntry | HealthCheckEntry;
